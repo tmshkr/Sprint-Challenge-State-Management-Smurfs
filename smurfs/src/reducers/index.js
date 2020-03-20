aefa131b69849ac34405fef1dc3a8e7307a4b18f
Tim Shaker <tim@tmshkr.com>
2020-03-20 08:19:02 -0700

    set up redux

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  smurfs: []
+};
+
+const reducer = (state = initialState, action) => {
+  const newState = { ...state };
+
+  switch (action.type) {
+    default:
+      break;
+  }
+  return newState;
+};
+
+export default reducer;
